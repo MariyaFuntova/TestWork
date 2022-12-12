d873cf1ae857aff6c96de45fd213163af8d82af0
Mariya <m_relaxeena@mail.ru>
2022-12-12 18:32:36 +0400

    task

---
+++ Testwork/task/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3 )
+        {
+            array2[count] = array1[i];
+            count++;
+        }     
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 3)
+        {
+            Console.Write(", ");
+        }    
+    }
+    Console.Write("]");
+}
+
+NewArray(array1,array2);
+PrintArray(array2);
